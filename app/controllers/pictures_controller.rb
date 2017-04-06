eb97c84705da4bed851b53f4de2f67afea4ff5a5
ilia24 <iliademertchian24@gmail.com>
2017-04-06 15:15:23 -0400

    Add show method to controller and views page for show path

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,8 @@ class PicturesController < ApplicationController
   def index
      @pictures = Picture.all
   end
+
+  def show
+    @picture = Picture.find(params[:id].to_i)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index' #match any GET request for for URL pictures with the index action in the picture controller
-  get 'pictures/:id' => 'pictures#show' as: 'picture' #links to the show method, and the "as picture" establishes this route as a picture_path
+  get 'pictures/:id' => 'pictures#show', as: 'picture' #links to the show method, and the "as picture" establishes this route as a picture_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
