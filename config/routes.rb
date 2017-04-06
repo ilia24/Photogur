e70ed5801616132b2726ada4c6e4d9cf13198091
ilia24 <iliademertchian24@gmail.com>
2017-04-06 11:31:31 -0400

    Add base index and picture class

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'pictures' => 'pictures#index'
+  get 'pictures' => 'pictures#index' #match any GET request for for URL pictures with the index action in the picture controller
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20170406150959_create_pictures.rb
@@ -0,0 +1,11 @@
+class CreatePictures < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pictures do |t|
+      t.string :artist
+      t.string :title
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
