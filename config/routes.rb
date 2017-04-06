89e1bf5997796dc1d2fee97e124a6fdd8d3715c8
ilia24 <iliademertchian24@gmail.com>
2017-04-06 16:16:31 -0400

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   #match any GET request for for URL pictures with the index action in the picture controller
+  root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
   #these two differentiate between the get request for a new picture page, and the post request to instantiate a new picture instance
