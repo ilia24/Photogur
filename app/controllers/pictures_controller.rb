457a19ee6891bc8be76967e7cbfacb2270d40a99
ilia24 <iliademertchian24@gmail.com>
2017-04-07 10:08:44 -0400

    Add destroy method in picture controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -26,12 +26,17 @@ class PicturesController < ApplicationController
 
   def update
     @picture = Picture.find(params[:id])
-
     if @picture.update_attributes(picture_params)
       redirect_to "/pictures/#{@picture.id}"
     else
       render :edit
     end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
   end
 
   private
